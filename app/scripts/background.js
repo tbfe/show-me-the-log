784f8309b2698dc29d833070d554965ccf41cc13
Lee Yeh <lee55962698@gmail.com>
2014-07-20 00:11:44 +0800

    sending response header to content console.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,6 +1,6 @@
 'use strict';
 
-filter = {
+var filter = {
     urls: ['*://*.baidu.com:*/*']
 };
 
@@ -15,12 +15,40 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
     };
 }, filter, ['blocking', 'requestHeaders']);
 
+//暂存区
+var staging = {};
+
 chrome.webRequest.onHeadersReceived.addListener(function(details) {
     //console.log('res: ', details);
+    var tabId = details.tabId;
+    var sendMessageToContent = function(message) {
+        chrome.tabs.sendMessage(tabId, message, function(response) {
+            if (response === undefined) {
+                //response不存在，则content script还未初始化，缓存之。
+                if (staging[tabId]) {
+                    staging[tabId].push(message);
+                } else {
+                    staging[tabId] = [message];
+                }
+            }
+        });
+    };
     for (var i = 0; i < details.responseHeaders.length; ++i) {
         if (details.responseHeaders[i].name === 'Content-Type') {
             console.log(details.responseHeaders[i]);
+            sendMessageToContent(details.responseHeaders[i]);
             break;
         }
     }
 }, filter, ['responseHeaders']);
+
+chrome.extension.onMessage.addListener(function(request, sender) {
+    var tabId = sender.tab.id;
+    console.log('tab ' + tabId + ' ready. sending staged messages');
+    if (staging[tabId]) {
+        for (var i = 0, length = staging[tabId].length; i < length; i++) {
+            chrome.tabs.sendMessage(tabId, staging[tabId][i]);
+        }
+        delete staging[tabId];
+    }
+});
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request);
+    if (sendResponse) {
+        sendResponse(true);
+    }
+});
+
+console.log('Show Me the LOG initialized.');
+
+chrome.extension.sendMessage('ready');
