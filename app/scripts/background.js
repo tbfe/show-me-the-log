27e94e20685a8a702ebc0075cb6999de5916d25f
Lee Yeh <lee55962698@gmail.com>
2014-07-19 18:24:22 +0800

    rewrite headers of request and response.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,26 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
-});
+filter = {
+    urls: ['*://*.baidu.com:*/*']
+};
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+    //console.log('req: ', details);
+    details.requestHeaders.push({
+        'name': 'X-Bingo-Dump-Log',
+        'value': '1'
+    });
+    return {
+        requestHeaders: details.requestHeaders
+    };
+}, filter, ['blocking', 'requestHeaders']);
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+chrome.webRequest.onHeadersReceived.addListener(function(details) {
+    //console.log('res: ', details);
+    for (var i = 0; i < details.responseHeaders.length; ++i) {
+        if (details.responseHeaders[i].name === 'Content-Type') {
+            console.log(details.responseHeaders[i]);
+            break;
+        }
+    }
+}, filter, ['responseHeaders']);
